69b5f2373cacaf038154876c81bdc1b74e81b88e
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-04-03 18:18:16 -0300

    main.go

--- voteService/go.mod
+++ voteService/go.mod
@@ -1,3 +1,9 @@
 module github.com/alexandrejastrow/big-voto/voteService
 
 go 1.18
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.4
+	github.com/streadway/amqp v1.0.0
+)
--- voteService/main.go
+++ voteService/main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"database/sql"
+
+	"github.com/alexandrejastrow/big-voto/voteService/envs"
+	"github.com/alexandrejastrow/big-voto/voteService/myerrors"
+	"github.com/alexandrejastrow/big-voto/voteService/pollservice"
+	"github.com/alexandrejastrow/big-voto/voteService/rabbitmq"
+	"github.com/joho/godotenv"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", req.URL.Path[1:])
+func conn() (*sql.DB, error) {
+	db_host := envs.GoDotEnvVariable("DB_HOST")
+	db_port := envs.GoDotEnvVariable("DB_PORT")
+	db_user := envs.GoDotEnvVariable("DB_USER")
+	db_pass := envs.GoDotEnvVariable("DB_PASS")
+	db_name := envs.GoDotEnvVariable("DB_NAME")
+
+	db, err := sql.Open("postgres", "host="+db_host+" port="+db_port+" user="+db_user+" password="+db_pass+" dbname="+db_name+" sslmode=disable")
+
+	return db, err
 }
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8050", nil)
+	godotenv.Load()
+
+	db, err := conn()
+
+	myerrors.FailOnError(err, "Failed to connect to Postgres")
+
+	defer db.Close()
+
+	conn_rabbitMQ, err := rabbitmq.Connection()
+
+	myerrors.FailOnError(err, "Failed to connect to RabbitMQ")
+
+	defer conn_rabbitMQ.Close()
+
+	go rabbitmq.Recive(conn_rabbitMQ, db)
+	go pollservice.Task()
+	forever := make(chan bool)
+
+	<-forever
+
 }
