741822aba37518eeb0c9ff440bd5a6ceb25382dc
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-04-04 19:36:29 -0300

    update settings file

--- bigvoto/app/settings/settings.py
+++ bigvoto/app/settings/settings.py
@@ -13,7 +13,6 @@ class AppSettings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     DEFAULT_MAX_TIME_TO_POLL: int
-    TIMEZONE: str
 
     class config:
         env_file = ".env"
